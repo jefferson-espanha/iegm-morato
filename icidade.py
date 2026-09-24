3a406922016ffea74484240d19d2e1d31478e9a4
Procuradoria <eduardoc451@gmail.com>
2026-09-24 08:47:09 -0300

    Update icidade.py

--- icidade.py
+++ icidade.py
@@ -2470,6 +2470,6 @@ def container_formulario_icidade(ano=None):
                             logging.exception("Erro no PDF:")
                             ui.notify(f"Erro ao gerar o PDF: {e}", type="negative", close_button=True)
 
-                    ui.button("📥 GERAR E ABRIR RELATÓRIO PDF", on_click=baixar_pdf).classes("bg-blue-700 text-white font-bold my-2")
+                    ui.button("📥 GERAR ABRIR RELATÓRIO PDF", on_click=baixar_pdf).classes("bg-blue-700 text-white font-bold my-2")
 
     render_conteudo()
